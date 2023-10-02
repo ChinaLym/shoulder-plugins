602dd6a5199edf881d4cc2cb1acba2d702427dd9
lym <lymdhr@qq.com>
2023-10-02 21:55:41 +0800

    add script

---
+++ changeCommitUserInfo.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="error_email"
+CORRECT_NAME="lym"
+CORRECT_EMAIL="lymdhr@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
